1ae7eb57fd73d270e8dc87d98d98a1ae62e3c021
Craig Johnson <craigdev937@gmail.com>
2024-10-07 16:18:23 -0400

    Initial setup of the Project.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Josh Tedeschi
+
+Initial setup of the webpage.  Created with React, TypeScript, Webpack, React Hook Forms, and Zod.
+
+
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>Josh's Webpage</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, adipisci. Vero ipsum eligendi tempora neque, error nostrum amet cupiditate! Hic nulla sequi molestias delectus minus, atque et ex minima provident!</p>
+        </React.Fragment>
+    );
+};
+
+
+
